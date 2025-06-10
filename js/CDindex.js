09d5fa4422479418c12c1a57183eef5fc474a285
wsahung <wsahung@connect.ust.hk>
2025-06-10 22:52:54 +0800

    Update CDindex.js

--- js/CDindex.js
+++ js/CDindex.js
@@ -193,9 +193,9 @@
                 tooltip_cd.html(`<strong>${d.title}</strong><br>
                             发表年份: ${d.year}<br>
                             CD指数: ${d.cdIndex.toFixed(3)}<br>
-                            被引用量(来自原始数据): ${d.cited_by_count}<br>
-                            仅引用本论文(来自截取数据集): ${d.cdData.ns}<br>
-                            同时引用本论文及至少一篇本论文的参考文献(来自截取数据集): ${d.cdData.np}`)
+                            被引用量: ${d.cited_by_count}<br>
+                            仅引用本论文: ${d.cdData.ns}<br>
+                            同时引用本论文及至少一篇本论文的参考文献: ${d.cdData.np}`)
                     .style("left", (event.pageX + 10) + "px")
                     .style("top", (event.pageY - 28) + "px");
             })
